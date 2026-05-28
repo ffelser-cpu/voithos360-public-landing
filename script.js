4b2bd22d0e9f10b1ff0165eefbed79c4842aaeb5
ffelser-cpu <ffelser@gmail.com>
2026-05-28 12:36:04 -0400

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-const demoVideo = document.querySelector(".video-placeholder video");
-const videoShell = document.querySelector(".video-placeholder");
-
-demoVideo?.addEventListener("canplay", () => {
-  videoShell?.classList.add("has-video");
-});
